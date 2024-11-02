40ac9c9c4b255bbc74d0789c0d57803f549d0b1e
Zytka <zyta.slowianska@gmail.com>
2024-11-02 18:37:09 +0100

    feat: getting into hooking into events

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
 #include "minilibx-linux/mlx.h"
+#include <X11/keysym.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define WIDTH			400
+#define HEIGHT			400
+#define MALLOC_ERROR	1
+
+typedef struct s_mlx_data
+{
+	void	*mlx_serv;
+	void	*mlx_wind;
+}	t_mlx_data;
+
+int	handle_input(int keysym, t_mlx_data *data);
 
 int	main(void)
 {
-	void	*mlx;
+	t_mlx_data	data;
 
-	mlx = mlx_init();
-	if (mlx == 0)
+	data.mlx_serv = mlx_init();
+	if (data.mlx_serv == 0)
+		return (MALLOC_ERROR);
+
+	data.mlx_wind = mlx_new_window(data.mlx_serv, HEIGHT, WIDTH, "My window");
+	if (data.mlx_wind == 0)
+	{
+		mlx_destroy_display(data.mlx_serv);
+		free(data.mlx_serv);
+		return (MALLOC_ERROR);
+	}
+	mlx_key_hook(data.mlx_wind, handle_input, &data);
+	mlx_loop(data.mlx_serv);
+	return (0);
+}
+
+int	handle_input(int keysym, t_mlx_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		printf("The %d key (ESC) has been pressed\n\n", keysym);
+		mlx_destroy_window(data->mlx_serv, data->mlx_wind);
+		mlx_destroy_display(data->mlx_serv);
+		free(data->mlx_serv);
 		return (1);
-
-	mlx_destroy_display(mlx);
-	free(mlx);
+	}
+	printf("The %d key has been pressed\n\n", keysym);
+	return (0);
 }
